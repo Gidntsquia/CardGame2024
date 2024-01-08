daf8e4e015d9a187a984126ed729db76917228f2
Jaxon Lee <jaxondlee@gmail.com>
2024-01-08 17:25:05 -0500

    Add monster active script

---
+++ Assets/Assets/Scripts/LaneBehavior.cs
@@ -0,0 +1,23 @@
+
+// Code by Jaxon Lee
+// 
+// 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaneBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Assets/Scripts/MonsterActive.cs
@@ -0,0 +1,72 @@
+// Code by Jaxon Lee
+//
+// Hold data for a monster that was summoned to the field. Tracks it's stats,
+// including current health.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterActive : MonoBehaviour
+{
+    private int basePower;
+    private int baseHealth;
+    private Image image;
+
+    public int power;
+    public int health;
+    public int powerBuffs;
+    public int healthBuffs;
+    public bool isDead = false;
+
+    // Initialize values
+    public void Initialize(int myBasePower, int myBaseHealth, Image myImage, int myPowerBuffs = 0, int myHealthBuffs = 0)
+    {
+        basePower = myBasePower;
+        baseHealth = myBaseHealth;
+        image = myImage;
+        powerBuffs = myPowerBuffs;
+        healthBuffs = myHealthBuffs;
+
+        // Calculate current power and health
+        power = basePower + powerBuffs;
+        health = baseHealth + healthBuffs;
+        isDead = false;
+    }
+
+    // Buff the monster
+    public void giveBuff(int powerBuff, int healthBuff)
+    {
+        powerBuffs += powerBuff;
+        healthBuffs += healthBuff;
+
+        power += powerBuff;
+        health += healthBuff;
+
+    }
+
+    // Reduce monster's health
+    public void reduceHealth(int damageToTake)
+    {
+        health -= damageToTake;
+        if (health <= 0)
+        {
+            // TODO: replace this with an event
+            isDead = true;
+        }
+    }
+
+
+    // ToString method to print values
+    public override string ToString()
+    {
+        return $"Monster Active Info:\n" +
+               $"Base Power: {basePower}\n" +
+               $"Base Health: {baseHealth}\n" +
+               $"Image: {image}\n" +
+               $"Power Buffs: {powerBuffs}\n" +
+               $"Health Buffs: {healthBuffs}\n" +
+               $"Current Power: {power}\n" +
+               $"Current Health: {health}\n";
+    }
+}
