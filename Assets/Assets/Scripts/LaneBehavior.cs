f0b6f3f0a93e7f43aeea75e1359ff70dd138b981
Jaxon Lee <jaxondlee@gmail.com>
2024-01-08 23:58:29 -0500

    Fix some lane behavior bugs

--- Assets/Assets/Scripts/LaneBehavior.cs
+++ Assets/Assets/Scripts/LaneBehavior.cs
@@ -28,6 +28,16 @@ public class LaneBehavior : MonoBehaviour
         doLaneCombat();
     }
 
+    private void Update()
+    {
+        // TODO: Remove this 
+        // Debugging command
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            doLaneCombat();
+        }
+    }
+
     // Manually create a hero and enemy monster for testing purposes.
     private void createTestMonsters()
     {
@@ -37,16 +47,17 @@ public class LaneBehavior : MonoBehaviour
         heroFront = heroMonster.GetComponent<MonsterActive>();
         heroFront.Initialize(heroTest);
 
-        GameObject enemyMonster = Instantiate(monsterPrefab);
-        enemyMonster.transform.SetParent(farMonsterSpot);
+        // GameObject enemyMonster = Instantiate(monsterPrefab);
+        // enemyMonster.transform.SetParent(farMonsterSpot);
 
-        enemyFront = enemyMonster.GetComponent<MonsterActive>();
-        enemyFront.Initialize(enemyTest);
+        // enemyFront = enemyMonster.GetComponent<MonsterActive>();
+        // enemyFront.Initialize(enemyTest);
 
     }
 
     // Do combat for this lane.
     // TODO: Turn this into an IEnumerator and make it a Coroutine f/ animations.
+    // TODO: Make herofront/back into an array of hero monsters.
     private void doLaneCombat()
     {
         // Note this order-- this is a key mechanic
@@ -59,33 +70,34 @@ public class LaneBehavior : MonoBehaviour
         enemyBack?.attack(heroFront, heroBack, heroHealthSystem);
 
         // Handle monster deaths
-        if (heroFront.isDead)
+        // TODO: Do this with events maybe
+        if (heroFront != null && heroFront.isDead)
         {
             Debug.Log($"Hero Front has died: {heroFront}");
             Destroy(heroFront.gameObject);
             heroFront = null;
         }
 
-        // if (heroBack.isDead)
-        // {
-        //     Debug.Log($"Hero Back has died: {heroBack}");
-        //     Destroy(heroBack.gameObject);
-        //     heroBack = null;
-        // }
+        if (heroBack != null && heroBack.isDead)
+        {
+            Debug.Log($"Hero Back has died: {heroBack}");
+            Destroy(heroBack.gameObject);
+            heroBack = null;
+        }
 
-        if (enemyFront.isDead)
+        if (enemyFront != null && enemyFront.isDead)
         {
             Debug.Log($"Enemy Front has died: {enemyFront}");
             Destroy(enemyFront.gameObject);
             enemyFront = null;
         }
 
-        // if (enemyBack.isDead)
-        // {
-        //     Debug.Log($"Enemy Back has died: {enemyBack}");
-        //     Destroy(enemyBack.gameObject);
-        //     enemyBack = null;
-        // }
+        if (enemyBack != null && enemyBack.isDead)
+        {
+            Debug.Log($"Enemy Back has died: {enemyBack}");
+            Destroy(enemyBack.gameObject);
+            enemyBack = null;
+        }
 
 
     }
