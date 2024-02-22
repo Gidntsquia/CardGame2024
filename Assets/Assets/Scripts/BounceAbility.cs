b153f71fb71eb034068b954995d1176b559af6a2
Jaxon Lee <jaxondlee@gmail.com>
2024-02-21 23:22:36 -0500

    Add bounce ability

---
+++ Assets/Assets/Scripts/BounceAbility.cs
@@ -0,0 +1,21 @@
+// Code written by Jaxon Lee
+// 
+// Data for a buff self card effect, which increases the power and health of the
+// monster.
+
+using UnityEngine;
+
+// This creates an menu entry in the Unity editor when you right click in the 
+// "Project" tab. It's called Abilities/newBounceAbility".
+[CreateAssetMenu(fileName = "newBounceAbility", menuName = "Abilities/BounceAbility", order = 3)]
+public class BounceAbility : CardAbility
+{
+
+    public override void OnPlay(Monster monsterToBounce)
+    {
+        // monsterToBounce.
+
+    }
+
+
+}
